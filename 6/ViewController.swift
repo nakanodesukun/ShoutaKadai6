7b42798b0c324478270eb4b86fb33211b6aa9ee4
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2022-01-05 15:24:15 +0900

    フィードバックをもとに修正しました

--- 6/ViewController.swift
+++ 6/ViewController.swift
@@ -5,54 +5,46 @@
 //  Created by 中野翔太 on 2021/12/28.
 //
 
-
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet private weak var slider: UISlider!
     @IBOutlet private weak var outputLabel: UILabel!
     @IBOutlet private weak var minimumLabel: UILabel!
     @IBOutlet private weak var maximumLabel: UILabel!
-    
-    //プロパティに値を保持
-    private var get = 0
+    // プロパティに値を保持
+    private var corectAnswer = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        numeric()
+        resetGame()
         minimumLabel.text = String(Int(slider.minimumValue))
         maximumLabel.text = String(Int(slider.maximumValue))
     }
-    
-    
-    
+
     @IBAction private func decisionButton(_ sender: Any) {
-        let slider = Int(slider.value)
-        if slider == get {
-            message(missing: "あたり!", value: slider)
+        let sliderValue = Int(slider.value)
+        if sliderValue == corectAnswer {
+            message(missing: "あたり!", value: sliderValue)
         } else {
-            message(missing: "はずれ!", value: slider)
+            message(missing: "はずれ!", value: sliderValue)
         }
-        
+
     }
-    
-    
-    private func numeric() {
-        //値をプロパティに格納
-        get = Int.random(in: 1 ... 100)
-        outputLabel.text = String(get)
+
+    private func resetGame() {
+        // 値をプロパティに格納
+        corectAnswer = Int.random(in: 1 ... 100)
+        outputLabel.text = String(corectAnswer)
         slider.setValue(50, animated: true)
     }
-    
+
     private func message(missing: String, value: Int) {
         let alert = UIAlertController(title: "結果", message: "\(missing)\nあなたの値\(value)", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "再挑戦", style: .default, handler: { UIAlertAction in
-            self.numeric()
-        } ))
+                                                                                   // メモリリークを防ぐため、クロージャーを弱参照
+        alert.addAction(UIAlertAction(title: "再挑戦", style: .default, handler: { [weak self] _ in
+            self?.resetGame()
+        }))
         present(alert, animated: true, completion: nil)
     }
 }
-
-
-
-
